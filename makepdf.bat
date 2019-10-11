029a188ba95ae13f7c85f33349aeb74eae8253dd
asdasd1dsadsa <835263969@qq.com>
2019-10-11 21:21:53 +0800

    Delete makepdf.bat

--- makepdf.bat
+++
@@ -1,6 +0,0 @@
-@cd build\latex\
-@pdflatex -interaction=nonstopmode HITwhPhysicsTournament.tex
-@makeindex -s python.ist HITwhPhysicsTournament.idx
-@pdflatex -interaction=nonstopmode HITwhPhysicsTournament.tex
-@cd ..
-@cd ..
